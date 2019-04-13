d3edef363675ee9468d0623779e9542fdbe1abd1
Jet132 <37117573+Jet132@users.noreply.github.com>
2019-04-13 11:15:34 +0200

    fixed date formatting

--- utils/mStatistics.ts
+++ utils/mStatistics.ts
@@ -149,7 +149,7 @@ export class MStatistics {
             webhooks: {}
         });
         this.hourly.doc.save();
-        console.log(`MStatistics hour updated at ${UTC}(${new Date(UTC).toISOString().replace(/[ZT]/g, " ")}) from ${oldHour.hour} to ${hour}`);
+        console.log(`MStatistics hour updated at ${UTC}(${new Date(UTC).toISOString().replace(/[ZT]/g, " ").slice(0, -1)}) from ${oldHour.hour} to ${hour}`);
     }
 
     /** changes summarises hour docs */
