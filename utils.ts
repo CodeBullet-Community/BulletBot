dd92e741fba618a9e3df1b14a0f6dd4109ff7cf8
Jet132 <37117573+Jet132@users.noreply.github.com>
2019-04-19 11:00:39 +0200

    revamp init commit

--- utils.ts
+++
@@ -1,7 +0,0 @@
-import * as filters from "./utils/filters";
-import * as messages from "./utils/messages";
-import * as parsers from "./utils/parsers";
-import * as permissions from "./utils/permissions";
-import {MStatistics} from "./utils/mStatistics";
-
-export default {filters,messages,parsers,permissions,MStatistics};
