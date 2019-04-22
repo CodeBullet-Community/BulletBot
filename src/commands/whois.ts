97b257484688590fe5f7a3dee17d8c671a67603f
Jet132 <37117573+Jet132@users.noreply.github.com>
2019-04-22 23:51:33 +0200

    fixed a loading issue

--- src/commands/whois.ts
+++ src/commands/whois.ts
@@ -1,29 +1,67 @@
-import {Message} from 'discord.js';
-import {MEMBER} from "../utils/permissions"
-import {Bot} from '..';
-import {commandInterface} from '../commands';
-let command:commandInterface = {
-	name:"whois",
-	path:"",
-	dm:false,
-	permLevel:MEMBER,
-	togglable:false,
-	shortHelp:"gives a description of a user",
-	embedHelp:async ():Promise<any>=>{
-		let prefix = await Bot.database.getPrefix();
-		return {
-			'embed':{
-				'color':Bot.database.settingsDB.cache.helpEmbedColor,
-				'author':{
-					name:'Command: '+ prefix + 'whois'
-				},
-				fields: [
-					
-				]
-			}
-		};
-	},
-	run:(message:Message,args:string,permLevel:number,dm:boolean,requestTimeStamp:number):Promise<void>=>{
+import { Message, RichEmbed, Guild } from 'discord.js';
+import { commandInterface } from '../commands';
+import { MEMBER } from '../utils/permissions';
+import { Bot } from '..';
+import { sendError } from '../utils/messages';
+import { permToString } from '../utils/parsers';
 
-	}
+var command: commandInterface = { name: undefined, path: undefined, dm: undefined, permLevel: undefined, togglable: undefined, shortHelp: undefined, embedHelp: undefined, run: undefined };
+
+
+command.run = async (message: Message, args: string, permLevel: number, dm: boolean, requestTimestamp: number) => {
+    try {
+        //Code here
+    } catch (e) {
+        sendError(message.channel, e);
+        Bot.mStats.logError();
+    }
+}
+
+command.name = 'whois';
+command.path = '';
+command.dm = true;
+command.permLevel = MEMBER;
+command.togglable = false;
+command.shortHelp = 'gives a description of a user';
+command.embedHelp = async function (guild: Guild) {
+    var prefix = await Bot.database.getPrefix(guild);
+    return {
+        'embed': {
+            'color': Bot.database.settingsDB.cache.helpEmbedColor,
+            'author': {
+                'name': 'Command: ' + prefix + command.name
+            },
+            'fields': [
+                {
+                    'name': 'Description:',
+                    'value': 'gives a description of a user'
+                },
+                {
+                    'name': 'Need to be:',
+                    'value': permToString(command.permLevel),
+                    'inline': true
+                },
+                {
+                    'name': 'DM capable:',
+                    'value': command.dm,
+                    'inline': true
+                },
+                {
+                    'name': 'Togglable:',
+                    'value': command.togglable,
+                    'inline': true
+                },
+                {
+                    'name': 'Usage:',
+                    'value': '{command}\n{command} [member]'.replace(/\{command\}/g, prefix + command.name)
+                },
+                {
+                    'name': 'Example:',
+                    'value': '{command}\n{command} @jeff#1234'.replace(/\{command\}/g, prefix + command.name)
+                }
+            ]
+        }
+    }
 };
+
+export default command;
