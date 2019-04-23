efa27e60357124b24a82ce6b05c3c04891017066
Jet132 <37117573+Jet132@users.noreply.github.com>
2019-04-23 21:27:30 +0200

    commented youtube.ts

--- src/youtube.ts
+++ src/youtube.ts
@@ -10,13 +10,23 @@ import xml2js = require('xml2js')
 import { sendMentionMessage } from './utils/messages';
 const xmlParser = new xml2js.Parser({ explicitArray: false });
 
+/**
+ * extracts youtube channel id using regex and the youtube_v3 api
+ *
+ * @export
+ * @param {string} input string where youtube channel id should be extracted
+ * @returns the extracted youtube channel id
+ */
 export async function getYTChannelID(input: string) {
+    // if input is https://www.youtube.com/channel/[channelID]
     if (input.includes("channel/")) {
         return input.split("channel/")[1];
     }
+    // if input is https://www.youtube.com/user/[username]
     if (input.includes("user/")) {
         input = input.split("user/")[1];
     }
+    // calls youtube api to look up username
     var response = await google.youtube("v3").channels.list({
         key: googleAPIKey,
         forUsername: input,
@@ -27,7 +37,15 @@ export async function getYTChannelID(input: string) {
     }
 }
 
+/**
+ * checks if youtube channel even exists
+ *
+ * @export
+ * @param {string} YTChannelID youtube channel id
+ * @returns if actually exists
+ */
 export async function YTChannelExists(YTChannelID: string) {
+    // calls youtube api to look up channel id
     var response = await google.youtube("v3").channels.list({
         key: googleAPIKey,
         id: YTChannelID,
@@ -37,42 +55,63 @@ export async function YTChannelExists(YTChannelID: string) {
     return false;
 }
 
+/**
+ * adds catcher layers to the given express application
+ * Layers:
+ *  - middelware layer for application/atom+xml parser
+ *  - get layer for verification
+ *  - post application for a actual callback
+ *
+ * @export
+ * @param {express.Application} app express application
+ */
 export function addYoutubeCatcher(app: express.Application) {
+    // middelware parser
     app.use(callbackPath + '/youtube', bodyParser.text({ type: 'application/atom+xml' }));
+    // get layer
     app.get(callbackPath + '/youtube', (req, res) => {
         res.status(200).send(req.query['hub.challenge']);
     });
+    // post layer
     app.post(callbackPath + '/youtube', function (req, res) {
+        // parse xml body
         xmlParser.parseString(req.body, async (error, result) => {
+            // checks if error occurred and if so return a 422 code 
             if (error) {
                 res.status(422).json({ code: 'xml_parse_error', details: "Something went wrong while parsing the XML", error });
             } else {
                 var video = result.feed.entry
+                // checks if a new video was published or a old one was updated
                 var publishUpdateDifference = Date.parse(video.updated) - Date.parse(video.published)
                 const type = (publishUpdateDifference > 300000) ? 'updated' : 'published'
+                // bot will only notify of new videos
                 if (type != "published") {
                     res.sendStatus(200);
                     return;
                 }
-                var webhooks = await Bot.youtube.webhooks.find({ feed: video["yt:channelId"] }).exec();
-                if (webhooks.length == 0) {
+                // gets webhook docs
+                var webhookDocs = await Bot.youtube.webhooks.find({ feed: video["yt:channelId"] }).exec();
+                if (webhookDocs.length == 0) {
                     res.sendStatus(404);
                     return;
                 }
 
                 const link = "https://youtu.be/" + video["yt:videoId"];
-                for (const webhook of webhooks) {
-                    const webhookObject: webhookObject = webhook.toObject();
+                // runs through all subscriptions for that youtube channel
+                for (const webhookDoc of webhookDocs) {
+                    const webhookObject: webhookObject = webhookDoc.toObject();
                     var guild = Bot.client.guilds.get(webhookObject.guild);
                     if (!guild) {
                         console.warn("in youtube webhookcatcher guild " + webhookObject.guild + " wasn't found");
-                        webhook.remove();
+                        webhookDoc.remove();
                     }
+                    // gets channel to send notification in
                     var channel: any = guild.channels.get(webhookObject.channel);
-                    if (!channel) webhook.remove();
+                    if (!channel) webhookDoc.remove();
                     var message: string = webhookObject.message;
+                    // replaces {{link}}, {{title}}, {{channelName}}, {{channelLink}}
                     message = message.replace("{{link}}", link).replace("{{title}}", video.title).replace("{{channelName}}", video.author.name).replace("{{channelLink}}", video.author.uri);
-                    if (message.includes("{{role:")) {
+                    if (message.includes("{{role:")) { // sends to special mention if needed
                         sendMentionMessage(guild, channel, message);
                     } else {
                         channel.send(message);
@@ -85,10 +124,34 @@ export function addYoutubeCatcher(app: express.Application) {
     });
 }
 
+/**
+ * manages all youtube webhooks. Connects to webhooks database and uses the youtube collection. Also sends requests to pubsubhubbub if needed.
+ *
+ * @export
+ * @class YTWebhookManager
+ */
 export class YTWebhookManager {
+    /**
+     * connection to webhooks database
+     *
+     * @type {mongoose.Connection}
+     * @memberof YTWebhookManager
+     */
     connection: mongoose.Connection;
+    /**
+     * model for youtube collection
+     *
+     * @type {mongoose.Model<webhookDoc>}
+     * @memberof YTWebhookManager
+     */
     webhooks: mongoose.Model<webhookDoc>;
 
+    /**
+     *Creates an instance of YTWebhookManager.
+     * @param {string} URI database URL with auth already in it
+     * @param {string} authDB name of auth database
+     * @memberof YTWebhookManager
+     */
     constructor(URI: string, authDB: string) {
         this.connection = mongoose.createConnection(URI + '/webhooks' + (authDB ? '?authSource=' + authDB : ''), { useNewUrlParser: true });
         this.connection.on('error', console.error.bind(console, 'connection error:'));
@@ -98,16 +161,32 @@ export class YTWebhookManager {
         this.webhooks = this.connection.model("youtubeWebhook", webhookSchema, "youtube");
     }
 
+    /**
+     * getter for webhooks with a webhookID
+     *
+     * @param {mongoose.Schema.Types.ObjectId} webhookID
+     * @returns
+     * @memberof YTWebhookManager
+     */
     get(webhookID: mongoose.Schema.Types.ObjectId) {
         return this.webhooks.findById(webhookID).exec();
     }
 
+    /**
+     * sends a post to pubsubhubbub to sub to a callback
+     *
+     * @private
+     * @param {string} YTChannelID channel to create a sub in
+     * @param {boolean} subscribe if to sub or unsub
+     * @returns
+     * @memberof YTWebhookManager
+     */
     private async subToChannel(YTChannelID: string, subscribe: boolean) {
         return new Promise((resolve, reject) => {
             request.post('https://pubsubhubbub.appspot.com/subscribe', {
                 form: {
                     'hub.mode': subscribe ? 'subscribe' : 'unsubscribe',
-                    'hub.callback': `https://${callbackURL}:${callbackPort}${callbackPath}/youtube`,
+                    'hub.callback': `https://${callbackURL}:${callbackPort}${callbackPath}/youtube`, // uses in bot-config specified callback URL
                     'hub.topic': 'https://www.youtube.com/xml/feeds/videos.xml?channel_id=' + YTChannelID
                 }
             }, (error, response, body) => {
@@ -120,9 +199,19 @@ export class YTWebhookManager {
         });
     }
 
+    /**
+     * creates webhook in database and also calls subToChannel if it0s the first time writing
+     *
+     * @param {string} guildID id of guild where the webhook should be made
+     * @param {string} channelID id for channel to witch the webhook should send
+     * @param {string} YTChannelID id of youtube channel to which to sub
+     * @param {string} message message that should get send on callback
+     * @returns created webhook doc
+     * @memberof YTWebhookManager
+     */
     async createWebhook(guildID: string, channelID: string, YTChannelID: string, message: string) {
-        var sameFeedWebhook = await this.webhooks.findOne({ feed: YTChannelID });
-        if (sameFeedWebhook && sameFeedWebhook.guild == guildID && sameFeedWebhook.channel == channelID) return undefined;
+        var sameFeedWebhook = await this.webhooks.findOne({ feed: YTChannelID }); // searches for a webhook with same feed to know if it should sub to subpubhubbub
+        if (sameFeedWebhook && sameFeedWebhook.guild == guildID && sameFeedWebhook.channel == channelID) return undefined; // checks if similar webhook already exists and if so then return
         if (!sameFeedWebhook) {
             try {
                 this.subToChannel(YTChannelID, true);
@@ -133,7 +222,7 @@ export class YTWebhookManager {
         }
         var guildDoc = await Bot.database.findGuildDoc(guildID);
         if (!guildDoc) return undefined;
-        var webhhookDoc = new this.webhooks({
+        var webhhookDoc = new this.webhooks({ // creates webhook doc
             feed: YTChannelID,
             guild: guildID,
             channel: channelID,
@@ -142,21 +231,30 @@ export class YTWebhookManager {
         await webhhookDoc.save();
         if (!guildDoc.webhooks) guildDoc.webhooks = {};
         if (!guildDoc.webhooks.youtube) guildDoc.webhooks.youtube = [];
-        guildDoc.webhooks.youtube.push(webhhookDoc.id);
+        guildDoc.webhooks.youtube.push(webhhookDoc.id); // adds webhook id to guild doc
         await guildDoc.save()
-        Bot.mStats.logWebhookAction('youtube', 'created');
+        Bot.mStats.logWebhookAction('youtube', 'created'); // logs action in mStats
         return webhhookDoc;
     }
 
+    /**
+     * deletes webhook form database and also unsubs from pubsubhubbub if it's the only webhook for specified channel
+     *
+     * @param {string} guildID guild where the webhook is
+     * @param {string} channelID channel where webhook sends notification to
+     * @param {string} YTChannelID id of youtube channel
+     * @returns deleted webhook doc
+     * @memberof YTWebhookManager
+     */
     async deleteWebhook(guildID: string, channelID: string, YTChannelID: string) {
-        var webhhookDoc = await this.webhooks.findOne({ feed: YTChannelID, guild: guildID, channel: channelID }).exec();
+        var webhhookDoc = await this.webhooks.findOne({ feed: YTChannelID, guild: guildID, channel: channelID }).exec(); // searches for exact webhook
         if (!webhhookDoc) return undefined;
         var guildDoc = await Bot.database.findGuildDoc(guildID);
         if (!guildDoc) return undefined;
-        guildDoc.webhooks.youtube.splice(guildDoc.webhooks.youtube.indexOf(webhhookDoc.id), 1);
+        guildDoc.webhooks.youtube.splice(guildDoc.webhooks.youtube.indexOf(webhhookDoc.id), 1); // removes webhook id of guild doc
         await guildDoc.save();
 
-        var sameFeedWebhookCount = await this.webhooks.countDocuments({ feed: YTChannelID }).exec();
+        var sameFeedWebhookCount = await this.webhooks.countDocuments({ feed: YTChannelID }).exec(); // counts webhooks with same feed to see if it need to unsub form pubsubhubbub
         if (sameFeedWebhookCount == 1) {
             try {
                 this.subToChannel(YTChannelID, false);
@@ -165,25 +263,38 @@ export class YTWebhookManager {
                 return null;
             }
         }
-        Bot.mStats.logWebhookAction('youtube', 'deleted')
+        Bot.mStats.logWebhookAction('youtube', 'deleted') // logs action in mStats
         return await webhhookDoc.remove();
     }
 
+    /**
+     * changes a property of a webhook.
+     * If the feed gets changed, this function will delete the older webhook and create a new one
+     *
+     * @param {string} guildID id of guild where existing webhook is in
+     * @param {string} channelID id of channel where the existing webhook sends notifications to
+     * @param {string} YTChannelID youtube channel id of existing webhook
+     * @param {string} [newChannelID] id of the new channel where to send notifications in
+     * @param {string} [newYTChannelID] id of new youtube channel to sub to
+     * @param {string} [newMessage] new message to send on callback
+     * @returns updated webhook doc
+     * @memberof YTWebhookManager
+     */
     async changeWebhook(guildID: string, channelID: string, YTChannelID: string, newChannelID?: string, newYTChannelID?: string, newMessage?: string) {
-        var webhhookDoc = await this.webhooks.findOne({ feed: YTChannelID, guild: guildID, channel: channelID }).exec();
+        var webhhookDoc = await this.webhooks.findOne({ feed: YTChannelID, guild: guildID, channel: channelID }).exec(); // gets exact webhook doc
         if (!webhhookDoc) return undefined;
         Bot.mStats.logWebhookAction('youtube', 'changed');
-        if (newYTChannelID) {
+        if (newYTChannelID) { // if there is a new feed it will delete the old one and create a new one
             var newWebhookDoc = await this.createWebhook(guildID, (newChannelID ? newChannelID : channelID),
-                newYTChannelID, (newMessage ? newMessage : webhhookDoc.toObject().message));
+                newYTChannelID, (newMessage ? newMessage : webhhookDoc.toObject().message)); // first try to create a new one
             if (!newWebhookDoc) return undefined;
             await this.deleteWebhook(guildID, channelID, YTChannelID)
             return newWebhookDoc;
         }
-        if (newChannelID) {
+        if (newChannelID) { // changes channel where notifications gets send in
             webhhookDoc.channel = newChannelID;
         }
-        if (newMessage) {
+        if (newMessage) { // changes message for notification
             webhhookDoc.message = newMessage;
         }
         webhhookDoc.save();
