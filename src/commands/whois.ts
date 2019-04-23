3ad90a244333bd1aec64eb08297d78a6b369645a
Jet132 <37117573+Jet132@users.noreply.github.com>
2019-04-23 13:20:51 +0200

    finished whois command

--- src/commands/whois.ts
+++ src/commands/whois.ts
@@ -1,31 +1,149 @@
-import { Message, RichEmbed, Guild } from 'discord.js';
+import { Message, RichEmbed, Guild, GuildMember } from 'discord.js';
 import { commandInterface } from '../commands';
-import { MEMBER } from '../utils/permissions';
+import { MEMBER, getPermissionLevel } from '../utils/permissions';
 import { Bot } from '..';
 import { sendError } from '../utils/messages';
-import { permToString } from '../utils/parsers';
+import { permToString, stringToMember } from '../utils/parsers';
+
+function getJoinRank(ID: string, guild: Guild) { // Call it with the ID of the user and the guild
+    if (!guild.member(ID)) return; // It will return undefined if the ID is not valid
+
+    let arr = guild.members.array(); // Create an array with every member
+    arr.sort((a, b) => a.joinedTimestamp - b.joinedTimestamp); // Sort them by join date
+
+    for (let i = 0; i < arr.length; i++) { // Loop though every element
+        if (arr[i].id == ID) return i; // When you find the user, return it's position
+    }
+}
+
+function ordinalSuffixOf(i: number) {
+    var j = i % 10,
+        k = i % 100;
+    if (j == 1 && k != 11) {
+        return i + "st";
+    }
+    if (j == 2 && k != 12) {
+        return i + "nd";
+    }
+    if (j == 3 && k != 13) {
+        return i + "rd";
+    }
+    return i + "th";
+}
+
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+function pad(n: any, width: number, padding?: string) {
+    padding = padding || '0';
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join(padding) + n;
+}
+
+function formatDate(date: Date) {
+    return `${days[date.getUTCDay()]} ${pad(date.getUTCDate(), 2)}/${pad(date.getUTCMonth() + 1, 2)}/${pad(date.getUTCFullYear(), 4)} ${pad(date.getUTCHours(), 2)}:${pad(date.getUTCMinutes(), 2)}:${pad(date.getUTCSeconds(), 2)}:${pad(date.getUTCMilliseconds(), 3)}`;
+}
+
+function getDayDiff(timestamp0: number, timestamp1: number) {
+    return Math.round(Math.abs(timestamp0 - timestamp1) / (1000 * 60 * 60 * 24));
+}
+
+function createMemberEmbed(member: GuildMember, permLevel: number) {
+    var date = new Date();
+    var roles = '';
+    var roleArray = member.roles.array();
+    roleArray.shift();
+    for (const role of roleArray) {
+        roles += role.toString() + ' ';
+    }
+    if (roles.length == 0) {
+        roles = 'member has no roles';
+    }
+
+    var joinRank: any = getJoinRank(member.id, member.guild) + 1;
+    if (joinRank == 1) {
+        joinRank = 'oldest member';
+    } else {
+        joinRank = ordinalSuffixOf(joinRank) + ' oldest member';
+    }
+    return {
+        "embed": {
+            "description": member.toString(),
+            "color": Bot.database.settingsDB.cache.defaultEmbedColor,
+            "timestamp": date.toISOString(),
+            "footer": {
+                "text": "ID: " + member.id
+            },
+            "thumbnail": {
+                "url": member.user.avatarURL
+            },
+            "author": {
+                "name": `${member.user.username} ${member.nickname ? '(' + member.nickname + ')' : ''}`,
+                "icon_url": member.user.avatarURL
+            },
+            "fields": [
+                {
+                    "name": "Status",
+                    "value": member.user.presence.status,
+                    "inline": true
+                },
+                {
+                    "name": "Join Rank",
+                    "value": joinRank,
+                    "inline": true
+                },
+                {
+                    "name": "Joined",
+                    "value": formatDate(member.joinedAt) + `\n (${getDayDiff(member.joinedTimestamp, date.getTime())} days ago)`,
+                    "inline": true
+                },
+                {
+                    "name": "Registered",
+                    "value": formatDate(member.user.createdAt) + `\n (${getDayDiff(member.user.createdTimestamp, date.getTime())} days ago)`,
+                    "inline": true
+                },
+                {
+                    "name": "Join-Register Diff",
+                    "value": getDayDiff(member.joinedTimestamp, member.user.createdTimestamp) + ' days',
+                    "inline": true
+                },
+                {
+                    "name": "Title",
+                    "value": permToString(permLevel),
+                    "inline": true
+                },
+                {
+                    "name": "Roles",
+                    "value": roles
+                }
+            ]
+        }
+    };
+}
+
+function sendMemberInfo(message: Message, member: GuildMember, permLevel: number, requestTimestamp: number) {
+    var embed = createMemberEmbed(member, permLevel)
+    Bot.mStats.logResponseTime(command.name, requestTimestamp);
+    message.channel.send(embed);
+    Bot.mStats.logCommandUsage(command.name, 'self');
+    Bot.mStats.logMessageSend();
+}
 
 var command: commandInterface = { name: undefined, path: undefined, dm: undefined, permLevel: undefined, togglable: undefined, shortHelp: undefined, embedHelp: undefined, run: undefined };
 
-
 command.run = async (message: Message, args: string, permLevel: number, dm: boolean, requestTimestamp: number) => {
-    /*
-			Note for whoever tries to implement this is that the following should be correct behavior
-			1) if a mention of a user is provided after the command that user should be the one that is gotten
-			2) if an exact name of a user is provided that is the user that should be gotten
-			3)if the id of a user is provided that is the user that should be gotten
-			4) if a name that doesn't exactly match any user is used then the user with the closest name should be chosen
-			5) if no info is provided it should whois the caller
-	*/
-	//A lot of methods used below don't exist yet and need to implemented
     try {
-        if(args.length === 0){
-        	message.reply(Bot.database.fetchUserInfo(message.author).toEmbed());
-        	return;
+        if (args.length === 0) {
+            sendMemberInfo(message, message.member, permLevel, requestTimestamp);
+            return;
         }
-        if(RegExp('<@([0-9])+>').test(args[0])){//will look for a string that has <@(some numbers)>
-        	
+
+        var member = stringToMember(message.guild, args);
+        if (!member) {
+            message.channel.send('Couldn\'t member with name ' + args);
+            Bot.mStats.logMessageSend();
+            return;
         }
+        sendMemberInfo(message, member, await getPermissionLevel(member), requestTimestamp);
     } catch (e) {
         sendError(message.channel, e);
         Bot.mStats.logError();
@@ -34,7 +152,7 @@ command.run = async (message: Message, args: string, permLevel: number, dm: bool
 
 command.name = 'whois';
 command.path = '';
-command.dm = true;
+command.dm = false;
 command.permLevel = MEMBER;
 command.togglable = false;
 command.shortHelp = 'gives a description of a user';
@@ -68,11 +186,11 @@ command.embedHelp = async function (guild: Guild) {
                 },
                 {
                     'name': 'Usage:',
-                    'value': `${prefix+command.name}\n${prefix+command.name} [member]`
+                    'value': `${prefix + command.name}\n${prefix + command.name} [member]`
                 },
                 {
                     'name': 'Example:',
-                    'value':`${prefix+command.name}\n${prefix+command.name} @Bullet Bot`
+                    'value': `${prefix + command.name}\n${prefix + command.name} @Bullet Bot#1234`
                 }
             ]
         }
