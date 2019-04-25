2780de74d0c2b601484dea2878e1b06fa6472d39
Jet132 <37117573+Jet132@users.noreply.github.com>
2019-04-25 21:37:58 +0200

    added command template

---
+++ src/commands/[command-name].ts
@@ -0,0 +1,93 @@
+// this file will be ignored by the compiler and is just to showcase how to make a command
+// those imports have to of course be changed depending on the path
+import { Message, Guild } from 'discord.js';
+import { commandInterface } from '../commands';
+import { permLevels } from '../utils/permissions';
+import { Bot } from '..';
+import { sendError } from '../utils/messages';
+import { permToString } from '../utils/parsers';
+
+var command: commandInterface = {
+    name: '[command name]', // command name must be lowercase letters with no spaces
+    path: '[path]', // if you don't want any path defined just do ''. example path 'cate/subcate'
+    dm: false, // if this command can be used in dms
+    permLevel: permLevels.mod,  // what minimum perm level is required
+    togglable: false, // if the command can be disabled by the commands command
+    shortHelp: '[short desc]', // very short desc of what the command does
+    embedHelp: async function (guild: Guild) {
+        var prefix = await Bot.database.getPrefix(guild);
+        return {
+            'embed': {
+                'color': Bot.database.settingsDB.cache.helpEmbedColor,
+                'author': {
+                    'name': 'Command: ' + prefix + command.name
+                },
+                'fields': [
+                    {
+                        'name': 'Description:',
+                        'value': '[detailed desc]' // more detailed desc
+                    },
+                    {
+                        'name': 'Need to be:',
+                        'value': permToString(command.permLevel),
+                        'inline': true
+                    },
+                    {
+                        'name': 'DM capable:',
+                        'value': command.dm,
+                        'inline': true
+                    },
+                    {
+                        'name': 'Togglable:',
+                        'value': command.togglable,
+                        'inline': true
+                    },
+                    {
+                        'name': 'Usage:', // all possible inputs to the guild, the arguments should be named
+                        'value': '{command} [arg]\n{command}'.replace(/\{command\}/g, prefix + command.name)
+                    },
+                    {
+                        'name': 'Example:', // example use of the command
+                        'value': '{command} fish\n{command}'.replace(/\{command\}/g, prefix + command.name)
+                    }
+                ]
+            }
+        }
+    },
+    run: async (message: Message, args: string, permLevel: number, dm: boolean, requestTime: [number, number]) => {
+        try {
+            // only put this here when the command always requires arguments
+            // if no argument is given then it will send the embed help
+            if (args.length == 0) {
+                message.channel.send(await command.embedHelp(message.guild));
+                Bot.mStats.logMessageSend();
+                return;
+            }
+            // if the command has several arguments use argIndex, as it's easier to implement optional arguments and new arguments
+            var argIndex = 0;
+            var argsArray = args.split(' ').filter(x => x.length != 0); // this ensures that double spaces get filtered out, so a typo won't make the command not work
+
+            // before the first message or response send (can also be in form of a reaction) `Bot.mStats.logResponseTime(command.name, requestTime);` should be called. 
+            // This will log the amount of time the bot needed on process the request, excluding the ping latency of course
+            Bot.mStats.logResponseTime(command.name, requestTime);
+
+            if (argsArray[argIndex] == 'hello') {
+                message.channel.send("hello there");
+                // every time a message is send, the 'Bot.mStats.logMessageSend();' function should be called
+                Bot.mStats.logMessageSend();
+                // every time the command was successfully executed the logCommandUsage should be called. 
+                //The first argument is always the command name, but the second one is OPTIONAL and is for specifying the sub command usage like 'list' or 'remove'
+                Bot.mStats.logCommandUsage(command.name, 'hello');
+            }else{
+                message.channel.send("bye");
+                Bot.mStats.logMessageSend();
+                Bot.mStats.logCommandUsage(command.name, 'bye');
+            }
+        } catch (e) {
+            sendError(message.channel, e);
+            Bot.mStats.logError();
+        }
+    }
+};
+
+export default command;
