97505daafde0492c9a6dc4dcb63a9a7a995e7dda
Montori <38043743+Montori@users.noreply.github.com>
2019-05-26 23:26:25 +0200

    T162: Added caselogging for every action

--- src/database/caseLogger.ts
+++ src/database/caseLogger.ts
@@ -1,7 +1,8 @@
 import mongoose = require('mongoose');
-import { caseDoc, caseSchema, caseObject } from './schemas';
-import { Guild, Role, User, GuildMember, Message, } from 'discord.js';
+import {caseDoc, caseSchema, caseObject, guildDoc, guildSchema} from './schemas';
+import {Guild, Role, User, GuildMember, Message, RichEmbed,} from 'discord.js';
 import { Bot } from '..';
+import {getDayDiff, timeFormat} from "../utils/time";
 
 export class CaseLogger{
 
@@ -12,6 +13,12 @@ export class CaseLogger{
      */
     connection: mongoose.Connection;
     /**
+     * model for guild collection
+     * @type {mongoose.Model<caseDoc>}
+     * @memberof CaseLogger
+     */
+    guilds: mongoose.Model<guildDoc>;
+    /**
      * model for case collection
      * @type {mongoose.Model<caseDoc>}
      * @memberof CaseLogger
@@ -21,7 +28,7 @@ export class CaseLogger{
      * Creates an instance of CaseLogger, connections to main database and inits all models.
      * @param {string} URI
      * @param {string} authDB
-     * @memberof Logger
+     * @memberof CaseLogger
      */
     constructor(URI: string, authDB: string) {
         var mainCon = mongoose.createConnection(URI + '/main' + (authDB ? '?authSource=' + authDB : ''), { useNewUrlParser: true });
@@ -33,23 +40,77 @@ export class CaseLogger{
             console.log('logger connected to /main database');
         });
         this.connection = mainCon;
+        this.guilds = mainCon.model('guild', guildSchema, 'guilds');
         this.cases = mainCon.model('cases', caseSchema, 'cases');
     }
 
     async logKick(guild: Guild, user: GuildMember, mod: GuildMember, reason?: string){
+        await this.logCase(guild, user, mod,'Kick', reason);
+    }
+
+    async logBan(guild: Guild, user: GuildMember, mod: GuildMember, reason?: string, duration?: number){
+        await this.logCase(guild, user, mod,'Ban', reason, duration);
+    }
+
+    async logSoftban(guild: Guild, user: GuildMember, mod: GuildMember, reason?: string){
+        await this.logCase(guild, user, mod,'Softban', reason);
+    }
+
+    async logUnban(guild: Guild, user: GuildMember, mod: GuildMember, reason?: string){
+        await this.logCase(guild, user, mod,'Unban', reason);
+    }
+
+    async logMute(guild: Guild, user: GuildMember, mod: GuildMember, reason?: string, duration?: number){
+        await this.logCase(guild, user, mod,'Mute', reason, duration);
+    }
+
+    async logUnmute(guild: Guild, user: GuildMember, mod: GuildMember, reason?: string){
+        await this.logCase(guild, user, mod,'Unmute', reason);
+    }
+
+    async logWarn(guild: Guild, user: GuildMember, mod: GuildMember, reason: string){
+        await this.logCase(guild, user, mod,'Warn', reason);
+    }
+
+    private async logCase(guild: Guild, user: GuildMember, mod: GuildMember, action: string, reason?: string, duration?: number){
         let date = new Date();
+        let guildDoc = await this.guilds.findOne({ guild: guild.id }).exec();
+        let logChannel: any = guild.channels.get(guildDoc.toObject().logChannel);
 
         let caseObject: caseObject = {
             guild: guild.id,
             caseID: Date.now(),
             user: user.id,
-            action: 'kick',
+            action: action,
             timestamp: date.getTime(),
-            //duration: 0,
+            duration: duration,
             mod: mod.id,
-            //reason: reason
+            reason: reason
         };
+
         let caseDoc = new this.cases(caseObject);
         await caseDoc.save();
+
+        if (!logChannel) return;
+        let caseEmbed = this.createCaseEmbed(user, mod, caseObject.caseID, action, duration);
+        logChannel.send(caseEmbed);
+
+
     }
+    private createCaseEmbed(user: GuildMember, mod: GuildMember, caseID: number, action: string, duration?: number, reason?: string){
+        var embed = new RichEmbed();
+        embed.setAuthor(`A case was created`);
+        embed.setFooter(`ID: ${caseID}`);
+        // @ts-ignore
+        embed.setTimestamp(date.toISOString());
+        embed.setColor(Bot.database.settingsDB.cache.embedColors.warn);
+        embed.addField('Type: ',action,true);
+        embed.addField("Mod: ", mod,true);
+        embed.addField("User: ", user,true);
+        if(duration) embed.addField("Duration: ", duration,true);
+        if(reason) embed.addField("Reason: ", reason,true);
+
+        return embed;
+    }
+
 }
