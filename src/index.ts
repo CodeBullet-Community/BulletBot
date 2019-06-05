938396cbcc01f41118e8061f119759c3f8defaf0
Jet132 <37117573+Jet132@users.noreply.github.com>
2019-06-05 20:38:21 +0200

    redid the on ready guild doc checks to also remove guilds

--- src/index.ts
+++ src/index.ts
@@ -122,11 +122,18 @@ setInterval(() => {
     }
 }, crashProof.interval);
 
-client.on('ready', () => {
-    Bot.client.user.setActivity('I\'m ready!');
-    for (const guild of client.guilds.array()) { // adds are guilds that were added while the bot was down
+client.on('ready', async () => {
+    let existingGuilds = await Bot.database.mainDB.guilds.distinct('guild').exec();
+    let guildsToRemove = existingGuilds.filter(x => !client.guilds.get(x));
+    let guildsToAdd = client.guilds.filter(x => !existingGuilds.includes(x.id));
+    console.info(`Adding ${guildsToAdd.size} guilds and removing ${guildsToRemove.length} guilds`);
+    for (const guildID of guildsToRemove) { // removes all guilds that the bot left while it was down
+        Bot.database.removeGuild(guildID)
+    }
+    for (const guild of guildsToAdd.array()) { // adds all guilds that the bot joined while it was down
         Bot.database.addGuild(guild.id);
     }
+    Bot.client.user.setActivity('I\'m ready!');
     console.log('I\'m ready!');
 });
 
