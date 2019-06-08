4d835042339cd2e545bd841ecbcbb9bec853cf89
Jet132 <37117573+Jet132@users.noreply.github.com>
2019-06-08 20:34:23 +0200

    added softban command (T169)

---
+++ src/commands/Moderation/softban.ts
@@ -0,0 +1,120 @@
+import { Message, Guild } from 'discord.js';
+import { commandInterface } from '../../commands';
+import { permLevels, getPermLevel } from '../../utils/permissions';
+import { Bot } from '../..';
+import { sendError } from '../../utils/messages';
+import { permToString, durationToString, stringToMember } from '../../utils/parsers';
+import { durations } from '../../utils/time';
+
+
+var command: commandInterface = {
+    name: 'softban',
+    path: '',
+    dm: false,
+    permLevel: permLevels.mod,
+    togglable: false,
+    cooldownLocal: durations.second,
+    shortHelp: 'Kick members and delete all their messages/reactions',
+    embedHelp: async function (guild: Guild) {
+        let prefix = await Bot.database.getPrefix(guild);
+        return {
+            'embed': {
+                'color': Bot.database.settingsDB.cache.embedColors.help,
+                'author': {
+                    'name': 'Command: ' + prefix + command.name
+                },
+                'fields': [
+                    {
+                        'name': 'Description:',
+                        'value': 'Ban and then immediately unban a member. This will kick the member and also delete all their reactions and messages from the last 7 days.' // more detailed desc
+                    },
+                    {
+                        'name': 'Need to be:',
+                        'value': permToString(command.permLevel),
+                        'inline': true
+                    },
+                    {
+                        'name': 'DM capable:',
+                        'value': command.dm,
+                        'inline': true
+                    },
+                    {
+                        'name': 'Togglable:',
+                        'value': command.togglable,
+                        'inline': true
+                    },
+                    {
+                        'name': 'Local Cooldown:',
+                        'value': durationToString(command.cooldownLocal),
+                        'inline': true
+                    },
+                    {
+                        'name': 'Usage:',
+                        'value': '{command} [member]\n{command} [member] [reason]'.replace(/\{command\}/g, prefix + command.name)
+                    },
+                    {
+                        'name': 'Example:',
+                        'value': '{command} @jeff#1234\n{command} @jeff#1234 for not being a good boi'.replace(/\{command\}/g, prefix + command.name)
+                    }
+                ]
+            }
+        }
+    },
+    run: async (message: Message, args: string, permLevel: number, dm: boolean, requestTime: [number, number]) => {
+        try {
+            if (args.length == 0) {
+                message.channel.send(await command.embedHelp(message.guild));
+                Bot.mStats.logMessageSend();
+                return false;
+            }
+            let argIndex = 0;
+            let argsArray = args.split(' ').filter(x => x.length != 0);
+
+            let member = await stringToMember(message.guild, argsArray[argIndex], false, false, false);
+            if (!member) {
+                message.channel.send('Couldn\'t find specified member');
+                Bot.mStats.logMessageSend();
+                return false;
+            }
+            if (member.user.id == message.author.id) {
+                message.channel.send('You can\'t softban yourself');
+                Bot.mStats.logMessageSend();
+                return false;
+            }
+            if (member.user.id == Bot.client.user.id) {
+                message.channel.send('You can\'t softban me :smiling_imp:');
+                Bot.mStats.logMessageSend();
+                return false;
+            }
+            if (await getPermLevel(member) >= permLevels.mod) {
+                message.channel.send('You can\'t softban that member');
+                Bot.mStats.logMessageSend();
+                return false;
+            }
+            if (!await message.guild.me.hasPermission("BAN_MEMBERS")) {
+                message.channel.send("I don\'t have the permissions to do that");
+                Bot.mStats.logMessageSend();
+                return false;
+            }
+
+            let reason = args.slice(args.indexOf(argsArray[0]) + argsArray[0].length).trim();
+
+            await Bot.caseLogger.logSoftban(message.guild, member, message.member, reason);
+            await member.send(`You were softbanned (kicked + deleting all your messages/reactions) from **${message.guild.name}** ${reason.length ? 'for:\n' + reason : ''}`);
+            await member.ban({ reason: reason, days: 7 });
+            message.guild.unban(member, 'auto unban for softban');
+
+            Bot.mStats.logResponseTime(command.name, requestTime);
+            message.channel.send(`:white_check_mark: **${member.user.tag} has been softbanned, ${reason}**`);
+            Bot.mStats.logCommandUsage(command.name);
+            Bot.mStats.logMessageSend();
+            return true;
+        } catch (e) {
+            sendError(message.channel, e);
+            Bot.mStats.logError(e, command.name);
+            return false;
+        }
+    }
+};
+
+export default command;
