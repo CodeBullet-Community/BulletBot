1e334f4dc7ca16d3175981ae71f767c21cd7febe
StrangeRanger <strangeranger01@gmail.com>
2019-09-16 08:20:04 +0000

    added code and is now usable by the master installer

---
+++ installers/linux/setup/mongodb-setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+clear
+read -p "We will now download and install MongoDB"
+echo "Importing public key"
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add - || {
+    echo "${red}Failed to import public key${nc}"
+    exit 1
+}
+echo "Creating MongoDB list file"
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" \
+    | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list || {
+        echo "${red}Failed to create MongoDB list file${nc}"
+        exit 1
+    }
+echo "Updating packages"
+apt update
+echo "Installing latest stable version of MongoDB"
+apt -y install mongodb-org
+echo ""
+read -p "${green}Finished installing MongoDB${nc}"
