8eb4f50674dfe852bf9c23f244cfcba751f9c8c3
StrangeRanger <strangeranger01@gmail.com>
2019-09-20 06:18:12 +0000

    the master installer is now in charge of setting up bulletbot.service

--- installers/linux/setup/autorestart-setup.sh
+++ installers/linux/setup/autorestart-setup.sh
@@ -1,16 +1,5 @@
-echo "[Unit]
-Description=A service to start BulletBot after a crash or server reboot
-After=network.target mongod.service
+#!/bin/bash 
 
-[Service]
-User=bulletbot
-ExecStart=/usr/bin/node /home/bulletbot/out/index.js
-Restart=always
-RestartSec=3
-
-[Install]
-WantedBy=multi-user.target
-" > /lib/systemd/system/bulletbot.service
 echo "[Unit]
 Description=A service to execute bullet-mongo-start.sh on server reboot
 
@@ -18,5 +7,6 @@ Description=A service to execute bullet-mongo-start.sh on server reboot
 ExecStart=/bin/bash /home/bulletbot/bullet-mongo-start.sh
 
 [Install]
-WantedBy=multi-user.target
-" > /lib/systemd/system/bullet-mongo-start.service
+WantedBy=multi-user.target" > /lib/systemd/system/bullet-mongo-start.service
+
+cp /home/bulletbot/installers/linux/autorestart/bullet-mongo-start.sh /home/bulletbot
