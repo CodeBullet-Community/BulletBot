4cdcc676fa05421e528771a7d5e5f05cd08e5184
StrangeRanger <strangeranger01@gmail.com>
2019-09-24 20:49:47 +0000

    updated output text, and now it removes or adds the files that the other startup modes use/need...

--- installers/linux/bb-start-modes/run-in-background-autorestart.sh
+++ installers/linux/bb-start-modes/run-in-background-autorestart.sh
@@ -1,18 +1,38 @@
-#!/bin/bash 
-  
+#!/bin/bash
+
+yellow=$'\033[1;33m'
+
 clear
 read -p "We will now run BulletBot in the background with auto restart on server reboot"
-
-echo "[Unit]
-Description=A service to execute bullet-mongo-start.sh on server reboot
-
-[Service]
-ExecStart=/bin/bash /home/bulletbot/bullet-mongo-start.sh
-
-[Install]
-WantedBy=multi-user.target" > /lib/systemd/system/bullet-mongo-start.service
-systemctl enable bullet-mongo-start.service
-
-cp /home/bulletbot/installers/linux/autorestart/bullet-mongo-start.sh /home/bulletbot
+echo "Creating files required to run Bulletbot with auto restart..."
+bash /home/bulletbot/installers/linux/autorestart/autorestart-updater.sh
 echo "Changing ownership of files added to the home directory..."
 chown bulletbot:admin -R *
+
+if [[ $bullet_status = "active" ]]; then
+    echo "${yellow}WARN: bulletbot.service is already running${nc}"
+    echo "Skipped starting bulletbot"
+    echo "${green}BulletBot is now set to run in the background with auto restart on server reboot${nc}"
+    read -p "Press [Enter] to continue to main installer menu"
+    exit 0
+fi
+
+echo "Starting bullebot.service..."
+systemctl start bulletbot.service || {
+    echo "${red}Failed to start bulletbot.service${nc}"
+    exit 1
+}
+# Waits in order to give bulletbot.service enough time to start
+echo "Waiting 20 seconds for bulletbot.service to start"
+sleep 20
+# Lists out the last 20 logs in order to better identify if and when
+# an error has occured during that start up of bulletbot.service
+echo -e "--------Last 20 lines of logged events for" \
+    "bulletbot.service---------\n$(journalctl -u bulletbot -n \
+    20)\n---------End of bulletbot.service logs--------\n"
+
+echo -e "Please check the logs above to make sure that there aren't any errors," \
+    "and if there, to resolve whatever issue is causing them\n"
+
+echo "${green}BulletBot is now running in the background with auto restart on server reboot${nc}"
+read -p "Press [Enter] to continue to main installer menu"
--- installers/linux/bb-start-modes/run-in-background.sh
+++ installers/linux/bb-start-modes/run-in-background.sh
@@ -1,22 +1,45 @@
 #!/bin/bash
 
-echo "Starting bulletbot.service..."
+yellow=$'\033[1;33m'
+timer=20
+
+clear
+read -p "We will now run Bulletbot in the background"
+
+if [[ $start_script_exists -eq 0 || $start_service_exists -eq 0 ]]; then
+    echo "Removing file(s) used to run BulletBot with auto restart..."
+    rm /home/bulletbot/bullet-mongo-start.sh 2>/dev/null
+    rm /lib/systemd/system/bullet-mongo-start.sh 2>/dev/null
+fi
+
+if [[ $bullet_status = "active" ]]; then
+    echo "${yellow}WARN: bulletbot.service is already running${nc}"
+    echo -e "Skipped starting bulletbot\n"
+    echo "${green}BulletBot is now set to run in the background${nc}"
+    read -p "Press [Enter] to continue to main installer menu"
+    exit 0
+fi
+
+echo "Starting bullebot.service..."
 systemctl start bulletbot.service || {
     echo "${red}Failed to start bulletbot.service${nc}"
     exit 1
 }
-
-# Waits for bulletbot.service to start
-echo "Waiting 20 seconds for bulletbot.service to start"
-sleep 20
-
+# Waits in order to give bulletbot.service enough time to start
+echo "Waiting 20 seconds for bulletbot.service to start..."
+while (($timer >= 0)); do
+    echo -en "\r$timer seconds left"
+    sleep 1
+    ((timer-=1))
+done
 # Lists out the last 20 logs in order to better identify if and when
 # an error has occured during that start up of bulletbot.service
-echo -e "--------Last 20 lines of logged events for" \
+echo -e "\n\n--------Last 20 lines of logged events for" \
     "bulletbot.service---------\n$(journalctl -u bulletbot -n \
     20)\n---------End of bulletbot.service logs--------\n"
 
 echo -e "Please check the logs above to make sure that there aren't any errors," \
-    "and if there, to resolve whatever issue is causing them\n"
+    "and if there are, to resolve whatever issue is causing them\n"
 
 echo "${green}BulletBot is now running in the background${nc}"
+read -p "Press [Enter] to continue to main installer menu"
