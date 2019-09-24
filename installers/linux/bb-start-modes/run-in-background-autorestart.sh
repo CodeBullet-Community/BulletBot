a89daaa94357aee9e5836966d5edff59eec00f28
StrangeRanger <strangeranger01@gmail.com>
2019-09-24 20:54:05 +0000

    changed/updated output text

--- installers/linux/bb-start-modes/run-in-background-autorestart.sh
+++ installers/linux/bb-start-modes/run-in-background-autorestart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 yellow=$'\033[1;33m'
+timer=20
 
 clear
 read -p "We will now run BulletBot in the background with auto restart on server reboot"
@@ -11,7 +12,7 @@ chown bulletbot:admin -R *
 
 if [[ $bullet_status = "active" ]]; then
     echo "${yellow}WARN: bulletbot.service is already running${nc}"
-    echo "Skipped starting bulletbot"
+    echo -e "Skipped starting bulletbot\n"
     echo "${green}BulletBot is now set to run in the background with auto restart on server reboot${nc}"
     read -p "Press [Enter] to continue to main installer menu"
     exit 0
@@ -23,16 +24,20 @@ systemctl start bulletbot.service || {
     exit 1
 }
 # Waits in order to give bulletbot.service enough time to start
-echo "Waiting 20 seconds for bulletbot.service to start"
-sleep 20
+echo "Waiting 20 seconds for bulletbot.service to start..."
+while (($timer >= 0)); do
+    echo -en "\r$timer seconds left"
+    sleep 1
+    ((timer-=1))
+done
 # Lists out the last 20 logs in order to better identify if and when
 # an error has occured during that start up of bulletbot.service
-echo -e "--------Last 20 lines of logged events for" \
+echo -e "\n\n--------Last 20 lines of logged events for" \
     "bulletbot.service---------\n$(journalctl -u bulletbot -n \
     20)\n---------End of bulletbot.service logs--------\n"
 
 echo -e "Please check the logs above to make sure that there aren't any errors," \
-    "and if there, to resolve whatever issue is causing them\n"
+    "and if there are, to resolve whatever issue is causing them\n"
 
 echo "${green}BulletBot is now running in the background with auto restart on server reboot${nc}"
 read -p "Press [Enter] to continue to main installer menu"
