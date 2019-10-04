657ae486cce8cdaf654f7a43c561f0ac44e2191c
StrangeRanger <strangeranger01@gmail.com>
2019-10-04 05:07:14 +0000

    fixed quotation (bug...), now gives the option to input googleAPIKey, instead of overwriting 'bot-config.json' it will be changed to 'bot-config.json.old' then creates new config file

--- installers/linux/setup/bot-config-setup.sh
+++ installers/linux/setup/bot-config-setup.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 clear
-echo "We will now set up bot-config.json"
-read -p "NOTE: This setup will only have you input the configurations that are \
-required for BulletBot to work. Any other settings/configurations must be added \
-manually (i.e. Google API Key)."
+read -p "We will now set up 'bot-config.json'"
 
 echo -e "\n-------------"
 while true; do
@@ -36,19 +33,29 @@ echo "-------------"
 echo "${cyan}NOTE: Depending on how MongoDB was set up (i.e. Authorization is" \
     "used), this field CAN be left empty${nc}"
 read -p "Enter the suffix to the MongoDB url (i.e. ?authSource=admin): " mongodb_url_suffix
-if [[ -z $mongodb_url_suffix ]]; then mongodb_url_suffix=""; fi
 echo "MongoDB url suffix: $mongodb_url_suffix"
+if [[ -z $mongodb_url_suffix ]]; then mongodb_url_suffix=""; fi
+echo -e "-------------\n"
+
+echo "-------------"
+echo "${cyan}NOTE: Can be left empty${nc}"
+read -p "Enter the Google API Key: " google_api_key
+echo "Google API Key: $google_api_key"
+if [[ -z $google_api_key ]]; then
+    google_api_key=""
+    echo "${yellow}You will not be able to use commands that require access to google${nc}"
+fi
 echo -e "-------------\n"
 
 bot_version=$(jq .version package.json)
 json="{
-    \"version\": \"$bot_version\",
+    \"version\": $bot_version,
     \"botToken\": \"$bot_token\",
     \"cluster\": {
         \"url\": \"$mongodb_url\",
         \"suffix\": \"$mongodb_url_suffix\"
     },
-    \"googleAPIKey\": \"[optional Google API key]\",
+    \"googleAPIKey\": \"$google_api_key\",
     \"bugForm\": {
         \"url\": \"[url to google form]\",
         \"serverID\": 123,
@@ -101,13 +108,20 @@ else
     while true; do
         echo "${cyan}'bot-config.json' already exists${nc}"
         echo "Would you like to:"
-        echo "1. Overwrite existing 'bot-config.json'"
+        echo "1. Create new 'bot-config.json'"
         echo "2. Compare the two side by side"
         echo "3. Stop and return to master installer menu"
         read option
         case $option in
             1)
-                echo "Overwriting 'bot-config.json'..."
+                if [ -f out/bot-config.json.old ]; then
+                    echo "Overwriting current 'bot-config.json.old' with current" \
+                        "'bot-config.json..."
+                else
+                    echo "Changing current 'bot-config.json' to 'bot-config.json.old..."
+                fi
+                mv out/bot-config.json out/bot-config.json.old
+                echo "Creating new 'bot-config.json'..."
                 echo $json | jq . > out/bot-config.json || {
                     echo "${red}Failed to overwrite 'bot-config.json' with" \
                         "human-readable JSON format${nc}" >&2
@@ -145,4 +159,31 @@ fi
 echo "Changing ownership of files added to the home directory..."
 chown bulletbot:admin -R *
 echo -e "\n${green}Finished setting up 'bot-config.json'${nc}"
+
+if [[ $bullet_status = "active" ]]; then
+    timer=20
+    echo "Restarting bulletbot.service to apply changes to 'bot-config.json..."
+    systemctl restart bulletbot.service || {
+        echo "${red}Failed to restart bulletbot.service${nc}" >&2
+        echo -e "\nExiting..."
+        exit 1
+    }
+
+    # Waits in order to give bulletbot.service enough time to start
+    echo "Waiting 20 seconds for bulletbot.service to start..."
+    while (($timer > 0)); do
+        echo -en "\r$timer seconds left"
+        sleep 1
+        ((timer-=1))
+    done
+    # Lists out the last 20 logs in order to better identify if and when
+    # an error has occured during that start up of bulletbot.service
+    echo -e "\n\n--------Last 20 lines of logged events for" \
+        "bulletbot.service---------\n$(journalctl -u bulletbot -n 20)" \
+        "\n---------End of bulletbot.service logs--------\n"
+
+    echo -e "Please check the logs above to make sure that there aren't any" \
+        "errors, and if there are, to resolve whatever issue is causing them\n"
+fi
+
 read -p "Press [Enter] to continue to master installer menu"
