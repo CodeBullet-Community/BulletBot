26f23c939763f1fad4c21ff4a260c9e5f3aa6544
Jet132 <37117573+Jet132@users.noreply.github.com>
2020-06-28 18:42:32 +0200

    implemented CommandCacheWrapper

--- src/database/schemas/main/commandCache.ts
+++ src/database/schemas/main/commandCache.ts
@@ -7,11 +7,47 @@ import { Schema } from "mongoose";
  * Holds cache data for commands in between messages from user
  */
 export interface CommandCache {
+    /**
+     * Channel the command originally was executed
+     *
+     * @type {(Snowflake | DMChannel | TextChannel)}
+     * @memberof CommandCache
+     */
     channel: Snowflake | DMChannel | TextChannel;
+    /**
+     * User that executed the command
+     *
+     * @type {(Snowflake | User)}
+     * @memberof CommandCache
+     */
     user: Snowflake | User;
+    /**
+     * Command that was executed
+     *
+     * @type {(CommandName | commandInterface)}
+     * @memberof CommandCache
+     */
     command: CommandName | commandInterface;
+    /**
+     * Permission level command was executed at
+     *
+     * @type {number}
+     * @memberof CommandCache
+     */
     permLevel: number;
+    /**
+     * Cache saved by command
+     *
+     * @type {*}
+     * @memberof CommandCache
+     */
     cache: any;
+    /**
+     * When this cache expires
+     *
+     * @type {(number | Date)}
+     * @memberof CommandCache
+     */
     expirationTimestamp: number | Date;
 }
 /**
--- src/database/wrappers/main/commandCacheWrapper.ts
+++ src/database/wrappers/main/commandCacheWrapper.ts
@@ -1,12 +1,13 @@
-import { ChannelResolvable, DMChannel, TextChannel, User } from 'discord.js';
+import { ChannelResolvable, DMChannel, TextChannel, User, Client } from 'discord.js';
 import { Model } from 'mongoose';
 import { keys } from 'ts-transformer-keys';
 
 import { Bot } from '../../..';
-import { commandInterface, CommandResolvable } from '../../../commands';
+import { commandInterface, CommandResolvable, Commands } from '../../../commands';
 import { PermLevel } from '../../../utils/permissions';
 import { CommandCache, CommandCacheDoc, CommandCacheObject } from '../../schemas/main/commandCache';
 import { DocWrapper } from '../docWrapper';
+import { container } from 'tsyringe';
 
 /**
  * Wrapper for the CommandCache object and document so everything can easily be access through one object
@@ -18,7 +19,6 @@ import { DocWrapper } from '../docWrapper';
 export class CommandCacheWrapper extends DocWrapper<CommandCacheObject> implements CommandCache {
     private _channel: DMChannel | TextChannel;
     readonly channel: DMChannel | TextChannel;
-    private _user: User;
     readonly user: User;
     private _command: commandInterface;
     readonly command: commandInterface;
@@ -26,8 +26,16 @@ export class CommandCacheWrapper extends DocWrapper<CommandCacheObject> implemen
     readonly cache: any;
     readonly expirationTimestamp: number;
     private _expirationDate: Date;
+    /**
+     * When this cache expires as a data object
+     *
+     * @type {Date}
+     * @memberof CommandCacheWrapper
+     */
     readonly expirationDate: Date;
-    private readonly bot: Bot;
+
+    private readonly client: Client;
+    private readonly commandModule: Commands;
 
     /**
      * Creates an instance of CommandCacheWrapper with basic values to identify the CommandCache.
@@ -37,26 +45,19 @@ export class CommandCacheWrapper extends DocWrapper<CommandCacheObject> implemen
      * @param {User} user
      * @memberof CommandCacheWrapper
      */
-    constructor(model: Model<CommandCacheDoc>, bot: Bot, channel: ChannelResolvable, user: User) {
-        let channelObj = getTextBasedChannel(channel);
-        if (!channelObj)
-            throw new Error('Invalid channel type was provided. CommandCache channels can only be of type TextChannel or DMChannel.');
+    constructor(model: Model<CommandCacheDoc>, channel: DMChannel | TextChannel, user: User) {
+        super(model, { channel: channel.id, user: user.id }, keys<CommandCacheObject>());
+        this.setDataGetters(['channel', 'user', 'command']);
 
-        let obj = {
-            channel: channelObj.id,
-            user: user.id
-        };
-        super(model, { channel: channelObj.id, user: user.id }, obj, keys<CommandCacheObject>());
+        this.user = user;
+        this.client = container.resolve(Client);
+        this.commandModule = container.resolve(Commands);
 
-        this.bot = bot;
-
-        this.subToMappedProperty('channel').subscribe(channel => this._channel = getTextBasedChannel(channel));
-        this.subToMappedProperty('user').subscribe(async user => this._user = await this.bot.client.users.fetch(user));
-        this.subToMappedProperty('command').subscribe(command => this._command = this.bot.commands.get(command));
+        this.subToMappedProperty('channel').subscribe(channel => this._channel = <any>this.client.channels.resolve(channel));
+        this.subToMappedProperty('command').subscribe(command => this._command = this.commandModule.get(command));
         this.subToMappedProperty('expirationTimestamp').subscribe(timestamp => this._expirationDate = new Date(timestamp));
 
         this.setWrapperProperty('channel', () => this._channel);
-        this.setWrapperProperty('user', () => this._user);
         this.setWrapperProperty('command', () => this._command);
         this.setWrapperProperty('expirationDate', () => this._expirationDate);
     }
@@ -67,12 +68,11 @@ export class CommandCacheWrapper extends DocWrapper<CommandCacheObject> implemen
      * @param {CommandResolvable} command Command for the cache
      * @param {PermLevel} permLevel What permissions level this CommandCache executes with
      * @param {object} cache Object that should be cached
-     * @param {number} expirationTimestamp When it expires
-     * @returns The CommandCacheWrapper if it was successfully created
+     * @param {number} expirationTimestamp When cache expires
      * @memberof CommandCacheWrapper
      */
     async init(command: CommandResolvable, permLevel: PermLevel, cache: object, expirationTimestamp: number) {
-        let commandObj = this.bot.commands.resolve(command);
+        let commandObj = this.commandModule.resolve(command);
         if (!commandObj)
             throw new Error('CommandResolvable failed to resolve to command');
 
@@ -84,70 +84,46 @@ export class CommandCacheWrapper extends DocWrapper<CommandCacheObject> implemen
             cache: cache,
             expirationTimestamp: expirationTimestamp
         }, true);
-        if (!result) {
-            console.warn('CommandCache initialization failed');
-            return undefined;
-        }
-
-        return this;
-    }
-
-    /**
-     * Only returns TextChannels or DMChannels
-     *
-     * @param {ChannelResolvable} channel Channel to get and check
-     * @returns TextChannel or DMChannel
-     * @memberof CommandCacheWrapper
-     */
-    getTextBasedChannel(channel: ChannelResolvable) {
-        let channelObj = this.bot.client.channels.resolve(channel);
-        if (!(channelObj instanceof TextChannel || channelObj instanceof DMChannel)) return undefined;
-        return channelObj;
+        if (!result)
+            throw new Error('CommandCache initialization failed');
     }
 
     /**
      * Sets the cache for the CommandCache
      *
      * @param {*} cache Cache to set it to
-     * @returns The provided cache if operation was successful
      * @memberof CommandCacheWrapper
      */
     async setCache(cache: any) {
-        let query = { $set: { cache: cache } };
-        await this.update(query);
+        await this.update({ $set: { cache: cache } });
         let tempData = this.cloneData();
         tempData.cache = cache;
         this.data.next(tempData);
-        return cache;
     }
 
     /**
-     * Extends the expiration timestamp by n milliseconds.
+     * Extends or shortens the expiration timestamp by n milliseconds.
+     * This operation does not load the "expirationTimestamp" field
      *
-     * @param {number} milliseconds How many milliseconds to extend the expiration timestamp
-     * @returns The extended expiration timestamp if operation was successful
+     * @param {number} milliseconds Ms to extend or shorten the expiration timestamp
      * @memberof CommandCacheWrapper
      */
     async extendExpirationTimestamp(milliseconds: number) {
-        await this.load('expirationTimestamp');
-        return await this.setExpirationTimestamp(this.expirationTimestamp + milliseconds);
+        await this.update({ $inc: { expirationTimestamp: milliseconds } });
     }
 
     /**
      * Sets the expiration timestamp for the CommandCache
      *
      * @param {number} expirationTimestamp Timestamp to set it to
-     * @returns The provided timestamp if operation successful
      * @memberof CommandCacheWrapper
      */
     async setExpirationTimestamp(expirationTimestamp: number) {
-        let query = { $set: { expirationTimestamp: expirationTimestamp } };
-        await this.update(query);
+        await this.update({ $set: { expirationTimestamp: expirationTimestamp } });
         let tempData = this.cloneData();
         tempData.expirationTimestamp = expirationTimestamp;
         this.data.next(tempData);
         this.addLoadedFields(['expirationTimestamp']);
-        return expirationTimestamp;
     }
 
     /**
